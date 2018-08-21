a5eef9f3fe88acf179abd9eb68a299dea1e65397
practice <practice@infoservice.local>
2018-08-21 19:26:46 +0300

    added some more

---
+++ app/src/main/java/com/vovik/airtablepretender/table/TableModels/ColumnCells/ColumnNumberCellModel.java
@@ -0,0 +1,31 @@
+package com.vovik.airtablepretender.table.TableModels.ColumnCells;
+
+import android.app.Activity;
+import android.view.View;
+
+import com.google.firebase.database.DataSnapshot;
+import com.vovik.airtablepretender.table.TableControllers.CellController;
+import com.vovik.airtablepretender.table.TableViews.CellView;
+import com.vovik.airtablepretender.table.TableViews.ColumnCells.DateCellView;
+
+public class ColumnChoiceCellModel {
+
+    ColumnChoiceCellModel(){
+        super();
+
+    }
+    public View getView(Activity context, CellController controller){
+
+        CellView cv = new DateCellView(context, controller);
+
+        return cv.getView();
+
+    }
+    public void setData(DataSnapshot data){}
+    public void setData(String data){}
+    public String getData(){
+        return "";
+        //TODO
+    }
+    public void eraseData(){}
+}
